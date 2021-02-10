365969f4fc145d63b34c2aaf29640db19f236fdf
Matt <Matthew.White02@gmail.com>
2021-02-10 21:29:23 +0000

    Optimise code for fetching view objects

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -8,10 +8,13 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener { rollDice() }
@@ -20,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val randomInt = (1..6).random()
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when(randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
