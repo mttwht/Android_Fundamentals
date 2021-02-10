84496aa370d9bcd78728e93b2cc44050b84540a6
Matt <Matthew.White02@gmail.com>
2021-02-10 22:10:42 +0000

    CHALLENGE: add a second dice

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -8,22 +8,29 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image_1)
+        diceImage2 = findViewById(R.id.dice_image_2)
         val rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage(): Int {
         val randomInt = (1..6).random()
 
-        val drawableResource = when(randomInt) {
+        return when(randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,6 +38,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
     }
 }
