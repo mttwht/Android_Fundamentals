e4e442a307a8004c4ee2f0b6e40a9e689be49377
Matt <Matthew.White02@gmail.com>
2021-03-30 22:18:44 +0100

    Add an event handler to the button

--- AboutMe/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.android.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button)
+            .setOnClickListener {
+                addNickname(it)
+            }
+    }
+
+    fun addNickname(view: View) {
+        val nicknameEditText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = nicknameEditText.text
+
+        nicknameEditText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+//        hide the keyboard
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
